e4cb2ca637caea0dfab2d8e49e3686ef5c944f00
vortexx <108076105+agreeed@users.noreply.github.com>
2024-09-24 14:58:50 +0500

    Update AccessoryApi.lua

--- AccessoryApi.lua
+++ AccessoryApi.lua
@@ -9,8 +9,10 @@ local function new(class, t)
 end
 
 private.AccessoryStorage = {
-	["infyld"] = function()
-		local ins = new("ParticleEmitter", {Enabled = true})
+	["infyld"] = function(parent)
+		local ins = new("Attachment", {Parent = parent, Name = "MIDDLE"})
+		local CIRCLE = new("ParticleEmitter", {Parent = ins, Enabled = true, Name = "CIRCLE", Brightness = 1, LightEmission = 1, LightInfluence = 0, Orientation = Enum.ParticleOrientation.VelocityPerpendicular, Size = NumberSequence.new({NumberSequenceKeypoint.new(0, 0, 0), NumberSequenceKeypoint.new(0.375, 3, 1), NumberSequenceKeypoint.new(1, 4, 1)}), Texture = "rbxassetid://8383677680", Transparency = NumberSequence.new({NumberSequenceKeypoint.new(0, 1, 0), NumberSequenceKeypoint.new(0.25, 0.75, 0), NumberSequenceKeypoint.new(1, 1, 0), ZOffset = -1, LifeTime = NumberRange.new(0.5, 1), Rate = 5, Speed = NumberRange.new(0.001), SpreadAngle = NumberRange.new(-360, 360)})
+		
 		return ins
 	end
 }
@@ -28,11 +30,11 @@ public.Accessory = function(AccessoryName)
 	return (function(t, s) return {Name = private.Sanitize(s), Encoded = t[private.Sanitize(s)]} end)(private.AccessoryStorage, AccessoryName)
 end
 
-public.Decode = function(Accessory)
+public.Decode = function(Accessory, Parent)
 	assert(type(Accessory) == "table", `Invalid argument #1 (expected OpenAcsAccessory got {type(Accessory)})`)
 	assert(public.Validate(private.Sanitize(Accessory.Name)), `Invalid argument #1 (expected OpenAcsAccessory got {type(Accessory)})`)
 
-	return Accessory.Encoded()
+	return Accessory.Encoded(Parent)
 end
 
 return public
